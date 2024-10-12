6566037cb0e523308326b7dd0e8c17acc7f3dd77
Tojo07 <tsimalayh@berea.edu>
2024-10-12 16:45:41 -0400

    updated page to upload image

--- frontend/wellbite/src/App.jsx
+++ frontend/wellbite/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home";
 import UserMain from "./userMain";
+import Login from "./login";
 function App() {
   return (
    <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
     <Route index element={<Home/>}/>
     <Route path="/home" element={<Home/>}/>
     <Route path="/dashboard" element={<UserMain/>}/>
+    <Route path="/login" element={<Login/>}/>
    </Routes>
    </BrowserRouter>
   );
--- frontend/wellbite/src/login.jsx
+++ frontend/wellbite/src/login.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Home =()=>{
+const Login =()=>{
     return (
         <section className="w-screen h-screen bg-temp_food_bg bg-center bg-cover bg-clip-border">
     <div className="bg-[#212121] bg-opacity-[60%] h-full w-full">
       <div className="w-full h-[10%] bg-inhet sticky flex justify-between px-12 place-items-center">
         <h1 className="font-futura text-white text-4xl">WellBite</h1>
-      <Link to={'/dashboard'}><button className="hover:-translate-y-1 hover:bg-gray-200 hover:text-black transition border-2 hover:scale-110 bg-transparent rounded-lg px-4 py-2 text-white ease-in-out">Login</button></Link>
+      <Link to={'/dashboard'}><button className="hover:-translate-y-1 hover:bg-gray-200 hover:text-black transition border-2 hover:scale-110 bg-transparent rounded-lg px-4 py-2 text-white ease-in-out">Register</button></Link>
       </div>
-
       <div className="w-full h-[80%] place-items-center justify-center">
-        <div className="w-full h-[60%] flex justify-center place-items-center">
-          <p className="font-futura text-center text-gray-200 text-4xl">A Dietary Tracker that helps patients easily<br/>manage dietary restrictions by tracking meals,<br/> offering personalized dietary safety check. Stay<br/> on track and share progress with healthcare<br/> providers effortlessly.</p>
-        </div>
         <div className="w-full h-[30%] justify-center flex flex-col place-items-center">
           <h1 className="text-gray-200 text-2xl underline">Get Started</h1>
           <div className="w-[26%] h-full flex justify-between place-items-center">
@@ -27,4 +23,4 @@ const Home =()=>{
     );
 };
 
-export default Home;
+export default Login;
